41cf31d5c55ddaa0b73c2a4ebf74362e5af38f49
Darya Potapova <muljykina.darya@gmail.com>
2022-09-24 15:30:21 +0300

    Changes main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        int[] sumSales = {15, 25, 30, 100};// массив количества продаж
+        SalesManager salesManager = new SalesManager(sumSales);
+        System.out.println("Максимальное количество продаж:");
+        salesManager.max();
     }
 }
