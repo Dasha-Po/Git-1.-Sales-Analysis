95f7f7024ad7a00b4dafd792bdaa52eaeec244ae
Darya Potapova <muljykina.darya@gmail.com>
2022-09-30 21:15:37 +0300

    Create new function: min, averageNum

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -16,4 +16,27 @@ public class SalesManager {
         }
         return max;
     }
+
+    //функция, возвращающая минимальное значение в заданном массиве
+    public int min() {
+        int min = sales[0];
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    // расчёт обрезанного среднего - это среднее количество продаж среди всех продаж,
+    // за исключением самой максимальной и самой минимальной
+    public int averageNum() {
+        int result = 0;
+        for (int sale : sales) {
+            result += sale;
+        }
+        int max = max();
+        int min = min();
+        return (result - max - min) / (sales.length - 2);
+    }
 }
